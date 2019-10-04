f7ad7c735cc4476dfbe213148946f326d9084a9f
Dhiraj Upadhyaya <dup1966@gmail.com>
2019-10-04 08:32:40 +0530

    a

--- C1/array1.R
+++ C1/array1.R
@@ -1,6 +1,40 @@
 #arrays
 
+#built in arrays
+UCBAdmissions
+class(UCBAdmissions)
+dim(UCBAdmissions)
+#3 dimensions : 2 rows(Admit), 2 cols(Gender), 6 Matrix (Depts)
+
 #create Arrays
+#Management Institute - 3 Depts, 2 batches, 2 Genders  
+set.seed(123)
+(studentnos = sample.int(n=30:40, size=3*2*2, replace=T))
+length(studentnos)
+(institute1 = array(data=studentnos, dim=c(2,2,3)))
+(institute2 = array(data=studentnos, dim=c(2,2,3), dimnames = list(c('Year1','Year2'), c('Male','Female'), c('Dept1','Dept2', 'Dept3'))))
 
+dim(institute2)
+#multi dim storage improves readability
 
+#access array elements
+institute2
+institute2[1,1,1] #Year1, Male, Dept1
+institute2[1,1,]  #Year1, Male, All Depts
+institute2[2,,2] #Year2, All Genders, Dept2
+institute2[,,2]  #Dept2
 
+institute2[-1, ,1]  #skip Year1(ie only Year2), All Genders, Dept2
+institute2[,-1 ,] #all Females from each Year & Dept
+institute2
+
+institute2[c(1,2),c(1) ,-c(2) ] #what is this
+
+#sum of Dept1 & Dept2
+(mat1 = institute1[,,1])
+(mat3 = institute1[,,3])
+(mat1 + mat3)
+institute2[, , c(1,3)]
+apply(institute2[, , c(1,3)], MARGIN=c(1,2), sum)
+
+#end
--- C1/array2.R
+++ C1/array2.R
@@ -2,5 +2,22 @@
 
 #Manipulate Arrays
 
+#Management Institute - 3 Depts, 2 batches, 2 Genders  
+set.seed(123)
+(studentnos = sample.int(n=30:40, size=3*2*2, replace=T))
+length(studentnos)
+(institute2 = array(data=studentnos, dim=c(2,2,3), dimnames = list(c('Year1','Year2'), c('Male','Female'), c('Dept1','Dept2', 'Dept3'))))
+
+addmargins(A=institute2)
+#sum in the end
+addmargins(A=institute2, margin = 2)
+dimnames(institute2)
+apply(X=institute2, MARGIN=1, FUN=sum)  #Yearwise sum
+apply(X=institute2, MARGIN=2, FUN=sum) #Genderwise sum
+apply(X=institute2, MARGIN=3, FUN=sum) #Deptwise sum
+apply(X=institute2, MARGIN=c(1,2), FUN=sum)  #Year-Gender wise sum
+apply(X=institute2, MARGIN=c(1,3), FUN=sum) #Year-Dept wise sum
+apply(X=institute2, MARGIN=c(2,3), FUN=sum) #Gender-Dept wise sum
+
 
 
