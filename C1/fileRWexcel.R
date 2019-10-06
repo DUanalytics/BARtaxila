f53fe43cf42e31a665335db148054e15c3e1ce85
Dhiraj Upadhyaya <dup1966@gmail.com>
2019-10-06 09:42:19 +0530

    a

--- C1/fileRWexcel.R
+++ C1/fileRWexcel.R
@@ -5,8 +5,8 @@
 
 library(rJava)  # install
 #set this path to your java file: you will find the version of Java in location : C:\Program Files\Java
-#note \\ in path
-Sys.setenv(JAVA_HOME='C:\\Program Files\\Java\\jre1.8.0_221')
+#note \\ in path : C:\Program Files\Java\jre1.8.0_221
+#Sys.setenv(JAVA_HOME='C:\\Program Files\\Java\\jre1.8.0_221')
 
 library(xlsx)  #install.packages('xlsx')
 
@@ -21,17 +21,18 @@ dir(path='./data')  #see the files in data folder
 
 ?read.xlsx
 #read data from Excel sheet if you know sheetname
-data1 <- read.xlsx(file='./data/RtoExcel.xlsx', sheetName='mtcars')
+data1 <- read.xlsx(file='data/RtoExcel.xlsx', sheetName='mtcars')
 head(data1)
 #read data from Excel sheet if you don't sheet name, but index
-data2 <- read.xlsx(file='./data/RtoExcel.xlsx', sheetIndex=1)
+data2 <- read.xlsx(file='data/RtoExcel.xlsx', sheetIndex=1)
 head(data2)
 
+?write.xlsx
 #store another data into the sheet. Excel file should be closed at this stage
-write.xlsx(x=women, file='data/RtoExcel.xlsx', sheetName='women', col.names=T, row.names=F)
+write.xlsx(x=women, file='data/RtoExcel.xlsx', sheetName='women', col.names=T, row.names=F, append=T)
 data3 <- read.xlsx(file='./data/RtoExcel.xlsx', sheetName='women')
 head(data3)
 
 
-#there are features of this library : explore
+#there are more features of this library : explore
 #end here...
--- C1/fileReadWrite.R
+++ C1/fileReadWrite.R
@@ -1,31 +1,31 @@
-#read / write Data - from/to File
+#read / write Data : R to - from/to File (CSV, TXT, XLSX...)
 #data can read into R from many file types : most common is CSV
 
 #make sure you have a folder : data in your project directory
-#like c1, c2, c3, c4, c5
+#like c1, c2, c3, c4, c5 , data
 
 mtcars  #inbuilt datasets
 data()  #other datasets - opens in another tab
+CO2
 
 ?write.csv
 write.csv(x=mtcars, file='data/mtcars1.csv')
 write.csv(x=mtcars, file='data/mtcars2.csv', row.names = F)
 #save it data folder all csv files
 
-
+#excel : save to CSV  : explain
+#one sheet at a time : excel to CSV : not full sheet
 #read--------------------
 
 newmtcars1 = read.csv(file='data/mtcars1.csv')
 head(newmtcars1)
 newmtcars2 = read.csv(file='data/mtcars2.csv')
-head(newmtcars2)
-#no car names
-newmtcars1A = read.csv(file='data/mtcars1.csv', row.names = 1)
-head(newmtcars1A)
-#with rownames
+head(newmtcars2) #no car names
 
 
 #webCSV---------
+#this csv file is part of repository in csv format you can see it here...
+#https://github.com/DUanalytics/BARtaxila/blob/master/data/taxilaUsers.csv
 url1 = "https://raw.githubusercontent.com/DUanalytics/BARtaxila/master/data/taxilaUsers.csv"
 taxila = read.csv(file=url1)
 head(taxila)
@@ -37,18 +37,25 @@ head(taxila)
 #https://docs.google.com/spreadsheets/d/1Md_ro2t3M7nA9JMH1DsE12jfeX7qq-UPw6p8WQd6A2Y/edit#gid=216113907  # you can copy this url and open in browser to see data
 url2 = "https://docs.google.com/spreadsheets/d/1Md_ro2t3M7nA9JMH1DsE12jfeX7qq-UPw6p8WQd6A2Y/edit#gid=216113907"
 #url of gsheet
-library(gsheet)
+library(gsheet)  #install this library first
 denco = as.data.frame(gsheet2tbl(url2))
+options(scipen = 99)
 head(denco)
 
 #cannot store to public read only gsheets unless you setup google apps authentication
+
 #Read from TextFile : deliimted by space
 ?read.table
 faculties = read.table(file='data/textData.txt', sep='',header=T)
 #sepearator is space, columnames is first row
 faculties
 
+#create text file from R Studio and add few rows and columns
+#last row should end with enter key 
+student = read.table(file='data/student.txt', header=T)
+student
 
-#other types can also be used
-#eg excel, STATA, SAS 
+#other types can also be used #eg excel, STATA, SAS 
 #preferred CSV files only
+
+#end here
