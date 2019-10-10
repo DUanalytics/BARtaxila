5e0293482ea6bc208188e5fd910ae888455fa4bb
dupadhyaya <dup1966@gmail.com>
2019-10-10 18:06:30 +0530

    a

--- C2/gg-density1.R
+++ C2/gg-density1.R
@@ -1,10 +1,11 @@
 #ggplot - Density Plot
 #Continous Variables
+library(ggplot2)
+library(dplyr)
 
 df=mtcars
 catcols = c('cyl', 'vs', 'am', 'gear', 'carb')
 df[,catcols] = lapply(df[,catcols], as.factor)
-
 
 ggplot(df) + geom_density(aes(x = mpg, color = gear))
 
@@ -13,3 +14,8 @@ ggplot(df) + geom_density(aes(x = mpg, color = gear))
 ggplot(df) + geom_density(aes(x = mpg, fill = gear), alpha = 0.2)
 
 ggplot(df) + geom_density(aes(x = mpg, fill = gear), alpha = 0.2) + facet_grid(am ~ vs)
+
+# Plot
+g <- ggplot(df, aes(mpg))
+g + geom_density(aes(fill=cyl, alpha=0.8)) +   labs(title="Density plot",  subtitle="City Mileage Grouped by Number of cylinders",       caption="Source: mpg",       x="City Mileage", fill="# Cylinders")
+                 
--- C2/gg-treemap.R
+++ C2/gg-treemap.R
@@ -21,3 +21,8 @@ dftree$cyl = paste('Cyl', dftree$cyl)
 
 treemap(dftree, index=c("gear", "cyl"),  vSize="n", vColor="meanMPG",  type="value", format.legend = list(scientific = FALSE, big.mark = " "))
 #Gear 5 : Ratio : Cylinder 4:6:8 = 2:1:2 ; highest MPG of Cyl4 ie 28, lowest of 15
+
+(dftree2 <- df %>% group_by(gear) %>% summarise(n=n(), meanMPG = mean(mpg)))
+treemap(dtf=dftree2,index = c("gear"),vSize="n", vColor ="meanMPG", type='value')
+#gear4 cars have highest MPG - darkgreen
+#gear3 are highest in no : width is largest
