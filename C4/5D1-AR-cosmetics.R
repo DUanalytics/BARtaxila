64a1a9d147ad24a00d5a793e7957b9022225e19f
Dhiraj Upadhyaya <dup1966@gmail.com>
2019-11-17 07:52:07 +0530

    a

--- C4/5D1-AR-cosmetics.R
+++ C4/5D1-AR-cosmetics.R
@@ -38,9 +38,70 @@ inspect(rules3)
 plot(rules2)
 plot(rules2, method='grouped')
 plot(rules2, method='graph', control=list(type='items'))
+plot(rules2,method="two-key plot")
+#uses support and confidence on x and y-axis respectively. It uses order for coloring. The order is the number of items in the rule.
+plot(rules2[1:5], method = "graph",  engine = "htmlwidget")
+inspect(head(rules2, n=5, by="lift"))
+subRules2B <- head(rules2, n=5, by="lift")
+plot(subRules2B, method="paracoord")
 
+inspect(rules2[1:5])
 quality(rules2)
 options(scipen=2)
 rules2A <- round(quality(rules2),2)
 rules2A
 inspect(rules2)
+inspect(sort(rules2, by = "lift")[1:5])
+
+subset(rules2, items %in% "Blush")
+#-----------
+transData = as(cosmetics,'transactions')
+inspect(transData)
+itemFrequency(transData)
+itemFrequencyPlot(transData)
+itemFrequencyPlot(transData, topN=5)
+itemFrequencyPlot(transData, topN=5, type='absolute')
+
+frequentsets=eclat(transData,parameter=list(support=0.01,maxlen=10))
+frequentsets
+inspect(frequentsets[1:5])
+rules5 = apriori(transData, parameter=list(support=.2))
+inspect(rules5)
+dfrules5 <- as(rules5, 'data.frame')
+head(dfrules5)
+inspect(transData[1:3])
+image(transData[1:3])
+#subset(transData, Items %in% c('Blush') )
+  
+
+#transData from Df
+head(cosmetics[1:5])
+items
+set.seed(101)
+transID = sample(1:500, 1000, replace=T)  #let there be duplicates
+item = sample(items, size=1000, replace=T)
+
+#http://www.learnbymarketing.com/1043/working-with-arules-transactions-and-read-transactions/
+orders <- data.frame(transID, item)
+head(orders)
+write.csv(orders, file = "data/my_basket.csv", row.names = F)
+order_trans <- read.transactions(file = "data/my_basket.csv",  format = "single",  sep = ",",  cols=c("transID","item"),  rm.duplicates = T, header=T)
+inspect(order_trans)
+itemFrequencyPlot(order_trans, topN=10)
+itemFrequencyPlot(order_trans, topN=10, type='absolute')
+
+eclat(order_trans)
+freqItems = eclat(order_trans, parameter=list(support=0.01,maxlen=10))
+inspect(freqItems)
+
+rules8 = apriori(order_trans, parameter=list(support=.0025))
+inspect(rules8)
+plot(rules8)
+plot(rules8, method='grouped')
+plot(rules8, method='graph', control=list(type='items'))
+plot(rules8,method="two-key plot")
+#uses support and confidence on x and y-axis respectively. It uses order for coloring. The order is the number of items in the rule.
+plot(rules8[1:5], method = "graph",  engine = "htmlwidget")
+inspect(head(rules8, n=5, by="lift"))
+subRules8B <- head(rules8, n=5, by="lift")
+plot(subRules8B, method="paracoord")
